c746a987f608b8a8a198a1d95a51fe294a1d38dc
Asa Shepard <asa.shepard@gmail.com>
2023-10-16 23:00:48 -0400

    update linear regression

--- capacity.R
+++ capacity.R
@@ -1,5 +1,5 @@
 ercot <- read.csv("data.csv")
-last_hour <- tail(ercot$hourofday, n = 1)
-ercot <- ercot[ercot$hourofday > last_hour - 3 & ercot$hourofday < last_hour, ]
-lm.capacity <- lm(formula = Total.System.Capacity ~ hourofday + Current.Frequency + Instantaneous.Time.Error + Actual.System.Demand + Average.Net.Load + Total.Wind.Output + Total.PVGR.Output + Current.System.Inertia, data = ercot)
+last_milli <- tail(ercot$time, n = 1)
+ercot <- ercot[ercot$time > last_milli - 3600, ]  # 3600 = ~1 hour
+lm.capacity <- lm(formula = Total.System.Capacity ~ hourofday, data = ercot)
 summary(lm.capacity)
--- demand.R
+++ demand.R
@@ -1,5 +1,5 @@
 ercot <- read.csv("data.csv")
-last_hour <- tail(ercot$hourofday, n = 1)
-ercot <- ercot[ercot$hourofday > last_hour - 3 & ercot$hourofday < last_hour, ]
-lm.demand <- lm(formula = Actual.System.Demand ~ hourofday + Current.Frequency + Instantaneous.Time.Error + Total.System.Capacity + Average.Net.Load + Total.Wind.Output + Total.PVGR.Output + Current.System.Inertia, data = ercot)
+last_milli <- tail(ercot$time, n = 1)
+ercot <- ercot[ercot$time > last_milli - 3600, ]  # 3600 = ~1 hour
+lm.demand <- lm(formula = Actual.System.Demand ~ hourofday, data = ercot)
 summary(lm.demand)
